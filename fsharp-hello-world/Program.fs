87eb01c04fec90dd95a26862ab97a4ad91b458ab
slanto <slawek.antos@gmail.com>
2019-03-17 16:40:25 +0100

    hello world created

---
+++ fsharp-hello-world/Program.fs
@@ -0,0 +1,11 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let SayHello() = 
+    printfn "Hello"
+
+[<EntryPoint>]
+let main argv =
+    SayHello()
+    0 // return an integer exit code
