2bbfb0d9bea46b24eb811ce4eae70ebe0ff250e2
slanto <slawek.antos@gmail.com>
2019-03-20 15:30:57 +0100

    Forward pipe operator used

--- fsharp-hello-world/arrays-module.fs
+++ fsharp-hello-world/arrays-module.fs
@@ -14,6 +14,9 @@ let numbersStep = [|1..5..99|]
 
 let squares = [| for i in 0..99 do yield i * i|]
 
+let isEven n = 
+        n % 2 = 0
+
 let RandomFruits count =
     let r = System.Random()
     let fruits = [|"apple";"orange";"pear"|]
@@ -22,3 +25,30 @@ let RandomFruits count =
             let index = r.Next(3)
             yield fruits.[index]
     |]
+
+let RandomFruits2 count =
+    let r = System.Random()
+    let fruits = [|"apple";"orange";"pear"|]
+    Array.init count (fun _ -> 
+        let index = r.Next(3)
+        fruits.[index]
+    )
+
+let LikeSomeFruits fruits =
+        for fruit in fruits do
+                printfn "I like %s" fruit
+
+let evenSquares = Array.filter (fun x -> isEven x) squares
+
+let sortedFruits = Array.sort [| "pear"; "orange"; "apple"|]
+
+let PrintLongWords (words: string[]) =
+        let longWords : string [] = Array.filter (fun w -> w.Length > 8) words
+        let sortedLongWords = Array.sort longWords
+        Array.iter (fun w -> printfn "%s" w) sortedLongWords
+
+let PrintLongWords2 (words: string[]) =
+        words 
+        |> Array.filter (fun w -> w.Length > 4)
+        |> Array.sort
+        |> Array.iter (fun w -> printfn "%s" w)
