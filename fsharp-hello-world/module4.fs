67e9de583cdf2ee66222c6ad135e8e2bdf68dbaa
slanto <slawek.antos@gmail.com>
2019-03-20 16:49:11 +0100

    option type introduced

--- fsharp-hello-world/module4.fs
+++ fsharp-hello-world/module4.fs
@@ -1,7 +1,6 @@
 module Module4
 
 //Record types
-
 type Person = 
     {
          FirstName : string
@@ -18,3 +17,23 @@ let person2 = { person with FirstName = "Monica"}
 //Structural equality == content equality 
 let person3 = {FirstName = "Kelly"; LastName = "Melly"}
 let areEquals = person = person3
+
+//Option type
+
+type Company = 
+    {
+        Name : string
+        SalesTaxNumber : int option      
+    }
+
+let company1 = { Name = "Ball Enterprise"; SalesTaxNumber = None }
+let company2 = { Name = "Axy Dot"; SalesTaxNumber = Some 1234}
+
+let PrintCompany (company : Company) =
+    let taxNumberString =
+        match company.SalesTaxNumber with
+        | Some n -> sprintf " [%i]" n
+        | None -> ""
+    printfn "%s%s" company.Name taxNumberString
+
+//Discriminated Unions
