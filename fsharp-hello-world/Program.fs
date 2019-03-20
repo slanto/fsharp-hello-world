b8e5ea45d76500d7f9ac14175cf45fc1794006e2
slanto <slawek.antos@gmail.com>
2019-03-20 13:42:37 +0100

    Nesting function defined

--- fsharp-hello-world/Program.fs
+++ fsharp-hello-world/Program.fs
@@ -1,11 +1,17 @@
 ﻿// Learn more about F# at http://fsharp.org
 
-open System
 
 let SayHello() = 
     printfn "Hello"
 
+let PrintNumbers min max =
+    let square n = 
+        n * n
+    for x in min..max do
+        printfn "%i %i" x (square x)
+
 [<EntryPoint>]
 let main argv =
     SayHello()
+    PrintNumbers 1 20
     0 // return an integer exit code
