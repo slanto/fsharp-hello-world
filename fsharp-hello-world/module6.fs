726ffc2f5ca9370c3088bf0c631d6aa0c4ecfe78
slanto <slawek.antos@gmail.com>
2019-03-21 21:44:19 +0100

    oo type

---
+++ fsharp-hello-world/module6.fs
@@ -0,0 +1,12 @@
+namespace Module6
+
+//OO Type (class)
+
+type Person(firstname : string, lastname : string) =
+    member __.Firstname = firstname //insted of this. you can use __, self, etc.
+    member __.Lastname = lastname
+
+type MutablePerson(firstname : string, lastname : string) =
+    member val Firstname = firstname with get, set
+    member val Lastname = lastname with get, set
+
