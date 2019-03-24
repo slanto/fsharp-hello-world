c4a9a30437cceefc69f3032fe1b419ef30870c2f
slanto <slawek.antos@gmail.com>
2019-03-24 21:49:06 +0100

    doing work in constructor

--- fsharp-hello-world/module6.fs
+++ fsharp-hello-world/module6.fs
@@ -10,3 +10,15 @@ type MutablePerson(firstname : string, lastname : string) =
     member val Firstname = firstname with get, set
     member val Lastname = lastname with get, set
 
+
+open System;
+
+type SafePerson(firstname : string, lastname : string) =
+    let validateString str =
+        if String.IsNullOrWhiteSpace str then
+            raise (ArgumentException())
+    do
+        validateString firstname
+        validateString lastname
+    member val Firstname = firstname with get, set
+    member val Lastname = lastname with get, set    
