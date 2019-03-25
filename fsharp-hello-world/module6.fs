c2239ea776fc1174b59a7d73b080a03780c76e36
slanto <slawek.antos@gmail.com>
2019-03-25 21:00:10 +0100

    interface in f#

--- fsharp-hello-world/module6.fs
+++ fsharp-hello-world/module6.fs
@@ -22,3 +22,27 @@ type SafePerson(firstname : string, lastname : string) =
         validateString lastname
     member val Firstname = firstname with get, set
     member val Lastname = lastname with get, set    
+
+open System;
+
+type IPerson =
+    abstract member Firstname : string
+    abstract member Lastname : string
+    abstract member Fullname : string
+
+type PersonFromInterface(firstname : string, lastname : string) =
+    let validateString str =
+        if String.IsNullOrWhiteSpace str then
+            raise (ArgumentException())
+    do
+        validateString firstname
+        validateString lastname
+
+    interface IPerson with
+        member __.Firstname = firstname
+        member __.Lastname = lastname
+        member __.Fullname = sprintf "%s %s" firstname lastname
+
+// let person12 = PersonFromInterface("imie", "nazwisko")
+// person.Fullname -> error
+// (person :> IPerson).Fullname -> you need to cast to the interface first; :> upcast operator
